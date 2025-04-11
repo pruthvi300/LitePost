a63c0de81f3112b3afcec58e67a8a02c15544cc8
Pruthviraj kakade <pruthvi3.pk@gmail.com>
2025-04-11 11:58:29 +0530

    Project created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+
+  console.log('env ----', import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+    <div>
+      test
+    </div>
+    </>
+  )
+}
+
+export default App
